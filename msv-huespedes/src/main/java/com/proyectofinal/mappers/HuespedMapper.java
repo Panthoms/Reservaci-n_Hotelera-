a9f6fce235d1785d14e6e12a5fda1630d4cb77bb
Rodolfo <rodolfo.bautista.hdez05@gmail.com>
2026-04-27 12:00:36 -0600

    refactor: huespedMapper

---
+++ msv-huespedes/src/main/java/com/proyectofinal/mappers/HuespedMapper.java
@@ -0,0 +1,33 @@
+package com.proyectofinal.mappers;
+
+import com.proyectofinal.common.dto.HuespedRequest;
+import com.proyectofinal.common.dto.HuespedResponse;
+import com.proyectofinal.common.mappers.CommonMapper;
+import com.proyectofinal.entities.Huesped;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class HuespedMapper implements CommonMapper<HuespedRequest, HuespedResponse, Huesped> {
+    @Override
+    public Huesped requestAEntidad(HuespedRequest request) {
+        if(request == null) return null;
+
+        return Huesped.builder()
+                .nombre(request.nombre())
+                .apellidoPaterno(request.apellidoPaterno())
+                .apellidoMaterno(request.apellidoMaterno())
+                .email(request.email())
+                .telefono(request.telefono())
+                .tipoDocumento(request.tipoDocumento())
+                .numeroDocumento(request.numeroDocumento())
+                .nacionalidad(request.nacionalidad())
+                .build();
+    }
+
+    @Override
+    public HuespedResponse entidadAResponse(Huesped entidad) {
+        return null;
+    }
+}
